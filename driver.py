6122fed906a11d253422a28b1ef46fd7e3e0b1df
Spencer Hughes <103297706+Spencerhughes11@users.noreply.github.com>
2022-04-09 12:33:10 -0500

    Add files via upload

--- driver.py
+++ driver.py
@@ -1,1 +1,11 @@
-"pete sus"
+from executive import Executive
+
+def main():
+    file_name = input("Enter a file name: ")
+    executive = Executive(file_name)
+    executive.run()
+    executive.lists()
+
+if __name__ == '__main__':
+    main()
+
